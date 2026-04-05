2575f4f95cdeca6806d1fdd50ddd2643f1f647f4
josefhint755-netizen <josefhint755@gmail.com>
2026-04-06 00:27:47 +0700

    menambahkan class SayaMusicTrack

--- TP_MODUL6_103022400006/Program.cs
+++ TP_MODUL6_103022400006/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using TP_MODUL6_103022400006;
+
+namespace TP_MODUL6_103022400006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaMusicTrack track1 = new SayaMusicTrack("Hingga Tua Bersama");
+
+            track1.PrintTrackDetails();
+
+            track1.IncreasePlayCount(5000);
+            track1.IncreasePlayCount(2500);
+
+            track1.PrintTrackDetails();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TP_MODUL6_103022400006/SayaMusicTrack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TP_MODUL6_103022400006
+{
+    public class SayaMusicTrack
+    {
+        public int id;
+        public int playCount;
+        public string title;
+
+        public SayaMusicTrack(string title)
+        {
+            Random random = new Random();
+            id = random.Next(10000, 100000);
+            this.title = title;
+            playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("Detail Track");
+            Console.WriteLine("ID        : " + id);
+            Console.WriteLine("Title     : " + title);
+            Console.WriteLine("Play Count: " + playCount);
+            Console.WriteLine();
+        }
+    }
+}
